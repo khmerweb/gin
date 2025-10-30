f0e3a6eeb5b1de9d040c874d39af8e161779f400
khmerweb <vuthdevelop1@gmail.com>
2025-10-31 01:43:33 +0700

    Make it better

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
-// package main
+//package main
+
 package handler
 
 import (
@@ -7,9 +8,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var router = gin.Default()
+
 func Init() {
 	gin.SetMode(gin.ReleaseMode)
-	var router = gin.Default()
 	router.Static("/static", "./static")
 	router.LoadHTMLGlob("templates/**/*.html")
 	// Define your Gin routes here
@@ -22,13 +24,11 @@ func Init() {
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	var router = gin.Default()
 	Init()
 	router.ServeHTTP(w, r)
 }
 
 func main() {
-	var router = gin.Default()
 	Init()
 	router.Run(":8000")
 }
