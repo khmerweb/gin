f3d8b49b92ae269a0ad0d27ae8f51e445f3dc4ed
khmerweb <vuthdevelop1@gmail.com>
2025-10-31 00:01:37 +0700

    Make it better

--- main.go
+++ main.go
@@ -1,29 +1,10 @@
-package handler
+package main
 
 import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
-
-func Handler(w http.ResponseWriter, r *http.Request) {
-	gin.SetMode(gin.ReleaseMode)
-	router := gin.Default()
-	router.Static("/static", "./static")
-	router.LoadHTMLGlob("templates/**/*.html")
-	// Define your Gin routes here
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "home.html", gin.H{
-			"title":   "My Gin Website",
-			"message": "Welcome to the homepage!",
-		})
-	})
-
-	// Add more routes as needed
-	// router.POST("/api/data", yourHandlerFunction)
-
-	router.ServeHTTP(w, r)
-}
 
 func main() {
 	// Initialize Gin router with default middleware (Logger and Recovery)
