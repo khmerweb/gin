e09636ad8dff88d8b49fcb114107d627d9c12e70
khmerweb <vuthdevelop1@gmail.com>
2025-10-30 23:10:57 +0700

    First commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Initialize Gin router with default middleware (Logger and Recovery)
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	router.Static("/static", "./static")
+	router.LoadHTMLGlob("templates/**/*.html")
+
+	router.GET("/", func(c *gin.Context) {
+		// Render the "index.html" template and pass data
+		c.HTML(http.StatusOK, "home.html", gin.H{
+			"title":   "My Gin Website",
+			"message": "Welcome to the homepage!",
+		})
+	})
+
+	// Run the server on port 8000
+	router.Run(":8000")
+}
