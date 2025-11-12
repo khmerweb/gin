1de52bc80986b67abd6167266de909ca79d26363
khmerweb <vuthdevelop1@gmail.com>
2025-11-12 12:52:22 +0700

    Make it better

--- public/static/scripts/set-playlists.js
+++ public/static/scripts/set-playlists.js
@@ -339,12 +339,11 @@
         })
     }
         
-    
-window.YT.ready(function() {
-    if (window.YT) {
-        load()
-    } else {
-        window.onYouTubeIframeAPIReady = load
-    }
-})
-        
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function onYouTubeIframeAPIReady() {
+    load()
+} 
